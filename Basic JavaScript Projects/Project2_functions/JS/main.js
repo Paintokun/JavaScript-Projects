62cfb70ccc71353eed50722e062a6952f8da29a5
Paintokun <rojassanrick@gmail.com>
2025-06-30 13:53:10 -0700

    Project2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+// The msg behiond the button interaction
+function changeText() {
+  document.getElementById("message").textContent = "It is real!";
+}
+//combining two text together
+function myFunction() {
+    var sentence = "This sentence";
+    sentence += " is a long sentence.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
