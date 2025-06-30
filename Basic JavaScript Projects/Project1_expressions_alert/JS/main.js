5d934a1fbbfed0e7231eee6d552ead2e8577e9f9
Paintokun <rojassanrick@gmail.com>
2025-06-30 12:45:08 -0700

    adding these two for my project

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+//this is making variables VVV
+var A = "My mother once said \"dont be a pussy\"";
+
+var L = "Lofton is a cool guy"
+
+var B = "Concatendated" + " String"
+
+var sent1 = "Lofton is really nice guy,"
+
+var sent2 = "the nicest"
+
+// this is making pop ups on the site
+window.alert("Hello World");
+
+window.alert(A)
+//this is writing text on the site
+document.write("\"Be who you are and say what you feel,\""
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+document.write(B)
+
+document.write(L)
+
+document.write(1+1)
+
+document.write(sent1 + sent2)
